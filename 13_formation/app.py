ae26ac1c3522371f30af07672ba74f709477e1d8
Kenny <lli15@stuy.edu>
2018-09-30 03:05:02 -0400

    refactor 13

--- 13_formation/app.py
+++ 13_formation/app.py
@@ -7,13 +7,17 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/")
 def home():
     return render_template("input.html")
 
-@app.route("/auth")
+@app.route("/auth", methods = ["GET", "POST"])
 def authenticate():
-    return render_template("output.html", name = request.args['name'], method = request.method)
+    if request.method == "GET":
+        return render_template("output.html", name = request.args['name'], method = request.method)
+    else:
+        return render_template("output.html", name = request.form['name'], method = request.method)        
+
 
 if __name__ == "__main__":
     app.debug = True
